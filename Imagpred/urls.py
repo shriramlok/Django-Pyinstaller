ab3e8185fbe945589c4427c3d48c3f4ded0eadb8
Shriram Lokhande <shriram.girish@qure.ai>
2021-06-22 13:52:49 +0530

    removing tutorials

--- Imagpred/urls.py
+++ Imagpred/urls.py
@@ -19,6 +19,5 @@ from django.conf.urls import url, include
  
 urlpatterns = [ 
     path('admin/', admin.site.urls),
-    url(r'^', include('tutorials.urls')),
     path('',include('predAPI.urls')),
 ]
